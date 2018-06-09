be234b89287d40a7c80bc60ef7971cfa713579b0
Korstek <30828144+Korstek@users.noreply.github.com>
2018-06-09 17:18:18 +0200

    Add files via upload

--- f_rjmp.c
+++ f_rjmp.c
@@ -3,14 +3,14 @@
 #include "mem_abs.h"
 
 void F_RJMP(void){
-    printf("0x%04X[0x%04X]: RJMP PC+0x%04X+1\n", getPC(), getOpcode(), getOpcode() & 0x0FFF);
-
     if((getOpcode() & 0x0800)==0) {
-        setPC(getPC() + (getOpcode() & 0x0FFF) + 1);
-        printf("PC: 0x%04X\n", getPC());
+        printf("0x%04X[0x%04X]: RJMP PC+%d+1\n", getPC(), getOpcode(), (getOpcode() & 0x0FFF));
+        setPC(getPC() + (getOpcode() & 0x0FFF)+1);
     }
     else
+    {
+        printf("0x%04X[0x%04X]: RJMP PC%d+1\n", getPC(), getOpcode(), -(0x1000-(getOpcode() & 0x0FFF)));
         setPC(getPC()-(0x1000-(getOpcode() & 0x0FFF)) + 1);
-        printf("PC:0x%04X\n", getPC());
+    }
     addCounter(1);
 }
