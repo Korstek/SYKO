3af71662f5b991e7dcd3d988c8ac3ab09e1b7ab8
Korstek <30828144+Korstek@users.noreply.github.com>
2018-06-10 14:15:48 +0200

    Add files via upload

--- interpreter.c
+++ interpreter.c
@@ -7,32 +7,21 @@
 #include "interrupt.h"
 
 //naglowki poszczegolnych opcodow
-//void F_ADD1(void);
 void F_RJMP(void);
 void F_IN(void);
-void F_OUT(void);
-//void F_SBRS(void);
 void F_RET(void);
 void F_CBI(void);
 //...
 
 //wzorce opcodow
-//#define ID_ADD_R1_R2            0x01
-#define ID_OUT_N_IN             0x0B	//OUT: 1011 1xxx xxxx xxxx, IN: 1011 0xxx xxxx xxxx
+#define ID_IN                   0x0B	//OUT: 1011 1xxx xxxx xxxx, IN: 1011 0xxx xxxx xxxx
 #define ID_RJMP                 0x0C
-//#define ID_SBRS                 0x7F
 #define ID_RET_OR_CBI           0x09    //RET: 1001 0101 0000 1000, CBI: 1001 1000 xxxx xxxx
 //...
 
 
 void doInstr(CodeType T){
     switch((T & 0xF000)>>12){   //wy�uskanie w�a�ciwego kodu operacji
-        /*case ID_ADD_R1_R2:       //wywolac instrukcje ADD R1,R2
-            F_ADD1();
-            break;
-        case ID_SBRS:
-            F_SBRS();
-            break;*/
         case ID_RJMP:           //wywolac instrukcje RJMP
             F_RJMP();
             break;
@@ -42,11 +31,8 @@ void doInstr(CodeType T){
             else
                 F_CBI();
             break;
-        case ID_OUT_N_IN:        //wywolac instrukcje OUT lub IN
-            if((T & 0x0800)!=0)
-                F_OUT();
-            else
-                F_IN();
+        case ID_IN:        //wywolac instrukcje OUT lub IN
+            F_IN();
             break;
 
         //...
--- makefile
+++ makefile
@@ -1,28 +1,22 @@
 syko.exe: 
 	gcc -c mem_abs.c -o mem_abs.o
-	gcc -c f_add1.c -o f_add1.o
 	gcc -c f_in.c -o f_in.o
-	gcc -c f_out.c -o f_out.o
 	gcc -c f_ret.c -o f_ret.o
 	gcc -c f_rjmp.c -o f_rjmp.o
 	gcc -c f_cbi.c -o f_cbi.o
-	gcc -c f_sbrs.c -o f_sbrs.o
 	gcc -c interrupt.c -o interrupt.o
 	gcc -c interpreter.c -o interpreter.o
 	gcc -c spi.c -o spi.o
 	gcc -c gpio.c -o gpio.o
 	gcc -c main.c -o main.o
-	gcc mem_abs.o f_add1.o f_in.o f_out.o f_rjmp.o f_ret.o f_cbi.o f_sbrs.o interrupt.o interpreter.o main.o spi.o gpio.o -o syko.exe
+	gcc mem_abs.o f_in.o f_rjmp.o f_ret.o f_cbi.o interrupt.o interpreter.o main.o spi.o gpio.o -o syko.exe
 
 clean:
 	rm -f mem_abs.o
-	rm -f f_add1.o
 	rm -f f_in.o
-	rm -f f_out.o
 	rm -f f_rjmp.o
 	rm -f f_ret.o
 	rm -f f_cbi.o
-	rm -f f_sbrs.o
 	rm -f interrupt.o
 	rm -f interpreter.o
 	rm -f main.o
